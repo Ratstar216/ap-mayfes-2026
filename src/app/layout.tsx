5e423cf2fc161c9275566dabf176833747ec7dd8
Ratstar216 <130824137+Ratstar216@users.noreply.github.com>
2026-05-11 19:46:21 +0900

    fix: fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,31 @@
 import type { Metadata } from "next";
+import {
+  Big_Shoulders,
+  IBM_Plex_Sans_JP,
+  Zen_Maru_Gothic,
+} from "next/font/google";
 import "./globals.css";
+
+const ibmPlexSansJp = IBM_Plex_Sans_JP({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-main",
+  display: "swap",
+});
+
+const bigShoulders = Big_Shoulders({
+  subsets: ["latin"],
+  weight: ["300"],
+  variable: "--font-display",
+  display: "swap",
+});
+
+const zenMaruGothic = Zen_Maru_Gothic({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-detail",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "工学博覧会 2026",
@@ -14,7 +40,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body
+        className={`${ibmPlexSansJp.variable} ${bigShoulders.variable} ${zenMaruGothic.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
